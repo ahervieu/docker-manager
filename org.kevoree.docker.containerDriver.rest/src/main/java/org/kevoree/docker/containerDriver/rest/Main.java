a66bbc0e25b5a0158513e7145a3c035e639ab00b
ahervieu <aymeric.hervieu@gmail.com>
2015-01-28 15:04:44 +0100

    rezvert

--- org.kevoree.docker.containerDriver.rest/src/main/java/org/kevoree/docker/containerDriver/rest/Main.java
+++ org.kevoree.docker.containerDriver.rest/src/main/java/org/kevoree/docker/containerDriver/rest/Main.java
@@ -46,8 +46,8 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-      //  System.in.read();
-    //    server.shutdown();
+        System.in.read();
+        server.shutdown();
     }
 }
 
